79373d9061091011d01871de5ea4fdd3f21a5518
Darshan Nevgi <darshann@buffalo.edu>
2019-10-04 22:07:03 -0400

    Adding FleetDBType interface Implementation for string, long and double

--- tablestore/fleetDBType_test.go
+++ tablestore/fleetDBType_test.go
@@ -12,5 +12,33 @@ func TestSerialize(t *testing.T) {
 	//mySlice := []byte{64,14,3,45}
 	newInt := myInt.Deserialize(byteSlice, 0)
 	fmt.Println((*newInt))
+	
+	myFloat := Float{3.14444}
+	byteSlice = myFloat.Serialize()
+	fmt.Println(byteSlice)
+	//mySlice := []byte{64,14,3,45}
+	newFloat := myFloat.Deserialize(byteSlice, 0)
+	fmt.Println((*newFloat))
+	
+	myLong := Long{20000000000}
+	byteSlice = myLong.Serialize()
+	fmt.Println(byteSlice)
+	//mySlice := []byte{64,14,3,45}
+	newLong := myLong.Deserialize(byteSlice, 0)
+	fmt.Println((*newLong))
+	
+	myDouble := Double{3.14444454543546}
+	byteSlice = myDouble.Serialize()
+	fmt.Println(byteSlice)
+	//mySlice := []byte{64,14,3,45}
+	newDouble := myDouble.Deserialize(byteSlice, 0)
+	fmt.Println((*newDouble))
+	
+	myString := String{"Hello World"}
+	byteSlice = myString.Serialize()
+	fmt.Println(byteSlice)
+	//mySlice := []byte{64,14,3,45}
+	newString := myString.Deserialize(byteSlice, 0)
+	fmt.Println((*newString))
 }
 
